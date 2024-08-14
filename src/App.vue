aa7e4bc3d786963b31bb28ad4875031e4890dd9f
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-14 13:48:47 +0200

    toggle button for light and dark mode added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
-    <Header />
+  <div id="app" class="min-h-screen bg-gray-100">
+    <Header></Header> 
+    <div class="flex justify-center py-4">
+      <ThemeToggle />
+    </div>
     <main class="container mx-auto px-4 py-8">
       <router-view></router-view>
     </main>
   </div>
 </template>
 
+
 <script>
 import Header from "../src/components/Header.vue";
+import ThemeToggle from './components/ThemeToggle.vue'; // Import the ThemeToggle component
 
 export default {
   name: "App",
   components: {
     Header,
+    ThemeToggle, // Register the ThemeToggle component here
   },
 };
 </script>
+<style>
+@import './assets/styles/global.css'; /* Optional: Import global styles if any */
+
+body,
+html {
+  height: 100%;
+  display: grid;
+  background: linear-gradient(to bottom, #edb74c 0%, #d6733a 100%);
+}
+
+.night {
+  background: linear-gradient(to bottom, #265078 0%, #01071c 100%);
+}
+</style>
---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="wrapper">
+      <button class="toggle" @click="toggleTheme">
+        <div class="toggle-btn" :class="{ 'left-1/2 bg-blue-900': isNightMode, 'left-1/4 bg-yellow-500': !isNightMode }"></div>
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isNightMode: false,
+      };
+    },
+    methods: {
+      toggleTheme() {
+        this.isNightMode = !this.isNightMode;
+        if (this.isNightMode) {
+          document.body.classList.add("night");
+          document.documentElement.classList.add("night");
+        } else {
+          document.body.classList.remove("night");
+          document.documentElement.classList.remove("night");
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .wrapper {
+    margin: 0;
+    padding: 0;
+    position: relative;
+    transform: scale(0.4); /* Scale down by 85% */
+  }
+  
+  .toggle {
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+    width: 100.5px; /* 85% of 170px */
+    height: 50px;    /* 85% of 100px */
+    background-color: #a690be;
+    border-radius: 50px;
+    position: inline;
+  }
+  
+  .toggle-btn {
+    width: 39.5px;  /* 85% of 70px */
+    height: 35.5px; /* 85% of 70px */
+    position: absolute;
+    bottom: 12.75px; /* 85% of 15px */
+    transition: all 0.5s ease-in;
+    border-radius: 50%; /* Make the toggle button circular */
+  }
+  
+  /* Add styles for night mode */
+  .night {
+    background-color: #2c3e50; /* Example dark background */
+    color: #ecf0f1;            /* Example light text color */
+  }
+  </style>
+  
