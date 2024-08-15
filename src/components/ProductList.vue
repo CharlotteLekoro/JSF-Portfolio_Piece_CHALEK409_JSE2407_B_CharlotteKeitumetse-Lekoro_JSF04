429659ef7d22fabfb7bf063343cdd3709e0532e9
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-08-15 08:58:26 +0200

    changed the position of the button

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <!-- Conditionally render button only on home route -->
     <div v-if="isHomePage">
-      <p class="text-gray-700 mb-2">Product data has been saved to local storage.</p>
-      <button @click="toggleProducts" class="bg-blue-500 text-white px-4 py-2 rounded">
+      <button 
+        @click="toggleProducts" 
+        class="fixed bottom-0 left-0 bg-blue-500 text-white px-4 py-2 rounded"
+      >
         {{ displayProducts ? 'Hide Discounted Products' : 'Show Discounted Products' }}
       </button>
+      
+      <p class="text-gray-700 mb-2">Product data has been saved to local storage.</p>
     </div>
 
-    <!-- Display products if toggle is true and on home page -->
     <div v-if="displayProducts && isHomePage" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
       <div 
         v-for="product in products" 
